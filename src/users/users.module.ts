9533969b5b19b787d6a6fddfe1419e18f726191b
Muhammad Faheem Akhtar <muhammadfaheemakhtar.pk@gmail.com>
2021-01-11 23:23:05 +0500

    feat: Add hook to update reviews user data on profile update

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,43 @@ import { UsersService } from './users.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './users.schema';
 import { UsersController } from './users.controller';
+import { BusinessesModule } from '../businesses/businesses.module';
+import { BusinessesService } from '../businesses/businesses.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: User.name,
+        imports: [BusinessesModule],
+        useFactory: async (businessesService: BusinessesService) => {
+          const schema = UserSchema;
+          schema.post('updateOne', async function() {
+            try {
+              const query = this.getQuery();
+              const userUpdates = this._update.$set;
+
+              if (query?._id && userUpdates?.name) {
+                const ownerId = query._id;
+                const updates = {
+                  avatar: userUpdates.avatar,
+                  name: userUpdates.name,
+                };
+
+                await businessesService.updateReviewUser(ownerId, updates);
+              }
+            } catch (error) {
+              throw new Error('Failed to update user data in reviews.');
+            }
+          });
+          return schema;
+        },
+        inject: [BusinessesService],
+      },
+    ]),
+  ],
   providers: [UsersService],
   exports: [UsersService],
-  controllers: [UsersController]
+  controllers: [UsersController],
 })
 export class UsersModule {}
