d9e0c2c06c84668bb221687390c8b5192c847538
Muhammad Faheem Akhtar <muhammadfaheemakhtar.pk@gmail.com>
2021-02-04 23:35:29 +0500

    feat: Update sorting in get business api

--- src/businesses/businesses.controller.ts
+++ src/businesses/businesses.controller.ts
@@ -42,10 +42,10 @@ export class BusinessesController {
     @Query('ownerId') ownerId: string,
     @Query('longitude') longitude: number,
     @Query('latitude') latitude: number,
+    @Query('radius', new DefaultValuePipe(5000), ParseIntPipe) radius: number, // meters, default: 5 km 
     @Query('limit', new DefaultValuePipe(20), ParseIntPipe) limit: number,
     @Query('skip', new DefaultValuePipe(0), ParseIntPipe) skip: number,
     @Query('favorite', new DefaultValuePipe(false), ParseBoolPipe) favorite: boolean,
-    @Query('near', new DefaultValuePipe(false), ParseBoolPipe) near: boolean,
     @Query('popular', new DefaultValuePipe(false), ParseBoolPipe) popular: boolean,
     @Query('fields', new DefaultValuePipe([]), ParseArrayPipe) fields: [string]
   ): Promise<Business[]> {
@@ -87,21 +87,16 @@ export class BusinessesController {
       query['favorites.ownerId'] = user._id.toString();
     }
 
-    // Use a general location to sort by distance.
-    // if(near){
-    //   longitude = 0;
-    //   latitude = 0;
-    // }
-
     // Use user's geo location. Using the first address. Overrides general location.
-    if(near && user?.addresses?.length && user.addresses[0].location?.coordinates?.length){
+    if(user?.addresses?.length && user.addresses[0].location?.coordinates?.length){
       [longitude, latitude] = user.addresses[0].location.coordinates;
     }
 
     // Use the provided coordinates, overrides others coordinates.
     if(latitude && longitude) {
       options.geoLocation = {
-        coordinates: [Number(longitude), Number(latitude)]
+        coordinates: [Number(longitude), Number(latitude)],
+        maxDistance: radius
       }
     }
 
