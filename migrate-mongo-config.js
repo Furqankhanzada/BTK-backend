80ad30ada059d23151b6108d540820b076dc6afb
abdul-hameed <raza2022@gmail.com>
2021-06-30 20:43:31 +0500

    feat: used .env for github actions

--- migrate-mongo-config.js
+++ migrate-mongo-config.js
@@ -6,7 +6,7 @@
 // I already added both up and down in npm scripts and it up will also run before build
 
 // using dot env to load local env file variables and solve the isolated build issue
-require('dotenv').config({ path: '.env.development.local' })
+require('dotenv').config()
 
 const config = {
   mongodb: {
