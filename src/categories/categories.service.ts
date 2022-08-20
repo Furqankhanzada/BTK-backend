8eccb5bc5d9ff117d5aa12f6999d84d019ae260d
Muhammad Furqan <furqan.khanzada@gmail.com>
2022-08-20 23:39:24 +0500

    chore: update response

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -4,65 +4,64 @@ import { Category } from './category.schema';
 import { Model } from 'mongoose';
 import { CreateCategoryDto, UpdateCategoryDto } from './create-category.dto';
 import { BusinessesService } from '../businesses/businesses.service';
-import { UpdateWriteOpResult } from 'mongoose';
 
 @Injectable()
 export class CategoriesService {
-    constructor(@InjectModel(Category.name) private categoryModel: Model<Category>, private businessService: BusinessesService) {}
+  constructor(@InjectModel(Category.name) private categoryModel: Model<Category>, private businessService: BusinessesService) {}
 
-    async create(createCategoryDto: CreateCategoryDto): Promise<Category> {
-        const createdCategory = new this.categoryModel(createCategoryDto);
-        try {
-            return await createdCategory.save();
-        } catch (error) {
-            if (error.code === 11000) {
-                throw new ConflictException('Category with this name already exist!');
-            }
-            return error;
-        }
+  async create(createCategoryDto: CreateCategoryDto): Promise<Category> {
+    const createdCategory = new this.categoryModel(createCategoryDto);
+    try {
+      return await createdCategory.save();
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new ConflictException('Category with this name already exist!');
+      }
+      return error;
     }
+  }
 
-    async findAll({
-            query = {},
-            projection = {},
-            options = {}
-        } = {}
-    ): Promise<Category[]> {
-        return this.categoryModel.find(query, projection, { sort: { order: 1 }, ...options }).exec();
+  async findAll({
+                  query = {},
+                  projection = {},
+                  options = {}
+                } = {}
+  ): Promise<Category[]> {
+    return this.categoryModel.find(query, projection, { sort: { order: 1 }, ...options }).exec();
+  }
+
+  async findOne(_id: string): Promise<Category> {
+    return this.categoryModel.findOne({ _id }).exec();
+  }
+
+  async update(_id: string, updateCategoryDto: UpdateCategoryDto): Promise<any> {
+    try {
+      const category = await this.findOne(_id);
+
+      const updatedCategory = await this.categoryModel.updateOne({ _id }, updateCategoryDto).exec();
+
+      if(updateCategoryDto.name && updateCategoryDto.name !== category.name ){
+        await this.businessService.updateMany({ category: category.name }, { category: updateCategoryDto.name });
+      }
+
+      return updatedCategory;
+    } catch (error) {
+      return error;
     }
+  }
 
-    async findOne(_id: string): Promise<Category> {
-        return this.categoryModel.findOne({ _id }).exec();
+  async remove(_id: string): Promise<{ deletedCount?: number }> {
+    try {
+      const category = await this.findOne(_id);
+      const businessesUsingCategory = await this.businessService.findAll({ query: { category: category.name }, projection: { _id: 1 }});
+
+      if(businessesUsingCategory.length){
+        throw new ConflictException('Category is in use.');
+      }
+
+      return this.categoryModel.deleteOne({ _id }).exec();
+    } catch (error) {
+      return error;
     }
-
-    async update(_id: string, updateCategoryDto: UpdateCategoryDto): Promise<UpdateWriteOpResult> {
-        try {
-            const category = await this.findOne(_id);
-        
-            const updatedCategory = await this.categoryModel.updateOne({ _id }, updateCategoryDto).exec();
-
-            if(updateCategoryDto.name && updateCategoryDto.name !== category.name ){
-                await this.businessService.updateMany({ category: category.name }, { category: updateCategoryDto.name });
-            }
-
-            return updatedCategory;
-        } catch (error) {
-            return error;
-        }
-    }
-
-    async remove(_id: string): Promise<{ deletedCount?: number }> {
-       try {
-            const category = await this.findOne(_id);
-            const businessesUsingCategory = await this.businessService.findAll({ query: { category: category.name }, projection: { _id: 1 }});
-
-            if(businessesUsingCategory.length){
-                throw new ConflictException('Category is in use.');
-            }
-  
-            return this.categoryModel.deleteOne({ _id }).exec();
-       } catch (error) {
-           return error;
-       }
-    }
+  }
 }
