c0f11bb5f8ac845da945b4c7443eeaf1f3ab0af2
MuhammadAliGabol <aligabol2002@gmail.com>
2022-12-28 21:45:53 +0500

    chore: added create notification dto

--- src/notifications/dto/create-notification.dto.ts
+++ src/notifications/dto/create-notification.dto.ts
@@ -1,1 +1,32 @@
-export class CreateNotificationDto {}
+import { IsNotEmpty, IsOptional, IsString, IsUrl } from "class-validator";
+
+export class CreateNotificationDto {
+    @IsString()
+    @IsNotEmpty()
+    title: string;
+
+    @IsString()
+    @IsNotEmpty()
+    description: string;
+
+    @IsString()
+    @IsOptional()
+    image: string;
+
+    @IsString()
+    @IsOptional()
+    date: string;
+
+    @IsString()
+    @IsUrl({ require_protocol: true })
+    @IsOptional()
+    link: string;
+
+    @IsString()
+    @IsOptional()
+    type: string;
+
+    @IsString()
+    @IsOptional()
+    read: boolean;
+}
