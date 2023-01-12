5b6c5d37b970e24e4141464f89512d11657d71d6
MuhammadAliGabol <aligabol2002@gmail.com>
2023-01-12 15:01:51 +0500

    refactor: remove un used file and code in notification-users

--- src/notification-users/notification-users.controller.ts
+++ src/notification-users/notification-users.controller.ts
@@ -1,10 +1,7 @@
-import { Controller, Get, Post, Body, Put, Param, Delete, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Request } from '@nestjs/common';
 import { NotificationUsersService } from './notification-users.service';
 import { CreateNotificationUserDto } from './dto/create-notification-user.dto';
-import { UpdateNotificationUserDto } from './dto/update-notification-user.dto';
 import { JwtAuthGuard, OptionalJwtAuthGuard } from 'src/auth/jwt-auth.guard';
-import { Roles } from 'src/auth/roles.decorator';
-import { RolesGuard } from 'src/auth/roles.guard';
 
 @Controller('notification-users')
 export class NotificationUsersController {
@@ -25,31 +22,4 @@ export class NotificationUsersController {
   ) {
     return this.notificationUsersService.findAll(req.user._id);
   }
-
-  // @Get(':id')
-  // @UseGuards(JwtAuthGuard)
-  // findOne(
-  //   @Param('id') id: string,
-  //   @Request() req,
-  //   ) {
-  //   return this.notificationUsersService.findOne(id, req.user._id);
-  // }
-
-  // @Put(':id')
-  // @Roles('ADMIN')
-  // @UseGuards(RolesGuard)
-  // @UseGuards(JwtAuthGuard)
-  // update(
-  //   @Param('id') id: string, 
-  //   @Body() updateNotificationDto: UpdateNotificationUserDto) {
-  //   return this.notificationUsersService.update(id, updateNotificationDto);
-  // }
-
-  // @Delete(':id')
-  // @Roles('ADMIN')
-  // @UseGuards(RolesGuard)
-  // @UseGuards(JwtAuthGuard)
-  // remove(@Param('id') id: string) {
-  //   return this.notificationUsersService.remove(id);
-  // }
 }
--- src/notification-users/notification-users.service.ts
+++ src/notification-users/notification-users.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
 import { CreateNotificationUserDto } from './dto/create-notification-user.dto';
-import { UpdateNotificationUserDto } from './dto/update-notification-user.dto';
 import { NotificationUser } from './notification-users.schema';
 
 @Injectable()
@@ -24,23 +23,4 @@ export class NotificationUsersService {
   async findAll(userId: string) {
     return this.notificationUserModel.find({ $or: [{ userId }, { userId: { $exists: false } }] });
   }
-
-  // async findOne(id: string, ownerId: string) {
-  //   const notification = await this.notificationUserModel.findOne({ _id: id }).exec();
-  //   if (notification.ownerId && notification.ownerId == ownerId) {
-  //     return notification;
-  //   }
-
-  //   if (!notification.ownerId) {
-  //     return notification;
-  //   }
-  // }
-
-  // update(id: string, updateNotificationDto: UpdateNotificationUserDto): Promise<Notification> {
-  //   return this.notificationUserModel.updateOne({ _id: id }, updateNotificationDto).exec();
-  // }
-
-  // remove(id: string): Promise<{ deletedCount?: number }> {
-  //   return this.notificationUserModel.deleteOne({ _id: id }).exec();
-  // }
 }
