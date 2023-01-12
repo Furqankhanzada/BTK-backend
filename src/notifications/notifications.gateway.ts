44161c7c056e41f32f1502cbcc69b7b73f10e406
MuhammadAliGabol <aligabol2002@gmail.com>
2023-01-12 13:52:05 +0500

    chore: added notifications gateway and create notification method with socket server

---
+++ src/notifications/notifications.gateway.ts
@@ -0,0 +1,22 @@
+import { WebSocketGateway, SubscribeMessage, MessageBody, WebSocketServer } from '@nestjs/websockets';
+import { CreateNotificationDto } from './dto/create-notification.dto';
+import { NotificationsService } from '../notifications/notifications.service';
+import { Server } from 'socket.io';
+
+@WebSocketGateway()
+export class NotificationsGateway {
+  @WebSocketServer()
+  server: Server;
+
+  constructor(private readonly notificationsService: NotificationsService) {}
+
+  @SubscribeMessage('createNotification')
+  async create(@MessageBody() createNotificationsliveDto: CreateNotificationDto) {
+    const notification = await this.notificationsService.create(createNotificationsliveDto, '63ad4d42fc59a73638458de0');
+
+    this.server.emit('notification', { added: notification });
+
+    console.log('CREATE NOTIFICATION with Server Listener', notification);
+    return notification;
+  }
+}
--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -3,10 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Notification, NotificationSchema } from './notification.schema';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
+import { NotificationsGateway } from './notifications.gateway';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Notification.name, schema: NotificationSchema }])],
   controllers: [NotificationsController],
-  providers: [NotificationsService]
+  providers: [NotificationsService, NotificationsGateway]
 })
 export class NotificationsModule {}
