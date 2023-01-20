0bfddc8d87911f08ce8b258ecc9f615b296dd11c
MuhammadAliGabol <aligabol2002@gmail.com>
2023-01-20 19:36:41 +0500

    fix: firebase private key usage

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -33,7 +33,9 @@ export class NotificationsService {
     initializeApp({
       credential: cert({
         "projectId": `${process.env.FIREBASE_PROJECT_ID}`,
-        "privateKey": `${process.env.FIREBASE_PRIVATE_KEY}`,
+        "privateKey": `${process.env.FIREBASE_PRIVATE_KEY
+          ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n")
+          : undefined}`,
         "clientEmail": `${process.env.FIREBASE_CLIENT_EMAIL}`,
       }),
       databaseURL: `${process.env.FIREBASE_DATABASE_URL}`,
