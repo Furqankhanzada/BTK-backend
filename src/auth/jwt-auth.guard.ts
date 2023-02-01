4fc0f30536dae52cdca22cb6af78f7b7743b1c3b
MuhammadAliGabol <aligabol2002@gmail.com>
2023-02-01 15:35:09 +0500

    refactor: remove extra optional auth guard

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -3,10 +3,3 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') { }
-
-export class OptionalJwtAuthGuard extends AuthGuard('jwt') {
-	// Override handleRequest so it never throws an error
-	handleRequest(err, user, info, context) {
-		return user;
-	}
-}
