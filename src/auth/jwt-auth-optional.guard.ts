f380381eb8ed71072ae28e33888fc3c98911f37e
MuhammadAliGabol <aligabol2002@gmail.com>
2023-02-01 15:35:35 +0500

    refactor: remove unused keys from options jwt auth guard

--- src/auth/jwt-auth-optional.guard.ts
+++ src/auth/jwt-auth-optional.guard.ts
@@ -4,7 +4,7 @@ import { AuthGuard } from '@nestjs/passport';
 @Injectable()
 export class JwtAuthGuardOptional extends AuthGuard('jwt') {
   // Override handleRequest so it never throws an error
-  handleRequest(err, user, info, context) {
+  handleRequest(err, user) {
     return user;
   }
 }
