0ce87d9b7bb8f9c77a6d31670a642511afc4df08
Muhammad Furqan <furqan.khanzada@gmail.com>
2023-02-05 01:51:17 +0500

    chore: fix p key

--- src/notifications/push-notifications.service.ts
+++ src/notifications/push-notifications.service.ts
@@ -27,7 +27,11 @@ export class PushNotificationsService {
     initializeApp({
       credential: cert({
         projectId: `${process.env.FIREBASE_PROJECT_ID}`,
-        privateKey: `${process.env.FIREBASE_PRIVATE_KEY}`,
+        privateKey: `${
+          process.env.FIREBASE_PRIVATE_KEY
+            ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, '\n')
+            : undefined
+        }`,
         clientEmail: `${process.env.FIREBASE_CLIENT_EMAIL}`,
       }),
     });
