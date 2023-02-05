af7be47b7cff20d4be0c6d0a6336a0867531aaba
Muhammad Furqan <furqan.khanzada@gmail.com>
2023-02-05 14:42:06 +0500

    chore: change how to load GOOGLE_ACCOUNT_CREDENTIALS

--- src/notifications/push-notifications.service.ts
+++ src/notifications/push-notifications.service.ts
@@ -25,15 +25,7 @@ export interface PushNotificationMessage {
 export class PushNotificationsService {
   constructor() {
     initializeApp({
-      credential: cert({
-        projectId: `${process.env.FIREBASE_PROJECT_ID}`,
-        privateKey: `${
-          process.env.FIREBASE_PRIVATE_KEY
-            ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, '\n')
-            : undefined
-        }`,
-        clientEmail: `${process.env.FIREBASE_CLIENT_EMAIL}`,
-      }),
+      credential: cert(JSON.parse(process.env.GOOGLE_ACCOUNT_CREDENTIALS)),
     });
   }
 
