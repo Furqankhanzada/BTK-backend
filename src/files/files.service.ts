9bf80bc2e35f5c9603b241c83f3c0f7e6d1b0fd6
Muhammad Furqan <furqan.khanzada@gmail.com>
2023-02-09 12:38:50 +0500

    chore: fix indentation

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -4,24 +4,26 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class FilesService {
-  constructor(
-    private readonly configService: ConfigService
-  ) { }
+  constructor(private readonly configService: ConfigService) {}
 
   async uploadPublicFile(dataBuffer: Buffer, filename: string) {
     const s3 = new S3();
-    return s3.upload({
-      Bucket: this.configService.get('AWS_PUBLIC_BUCKET_NAME'),
-      Body: dataBuffer,
-      Key: filename
-    }).promise();
+    return s3
+      .upload({
+        Bucket: this.configService.get('AWS_PUBLIC_BUCKET_NAME'),
+        Body: dataBuffer,
+        Key: filename,
+      })
+      .promise();
   }
 
   async deletePublicFile(pathname: string) {
     const s3 = new S3();
-    return s3.deleteObject({
-      Bucket: this.configService.get('AWS_PUBLIC_BUCKET_NAME'),
-      Key: pathname
-    }).promise();
+    return s3
+      .deleteObject({
+        Bucket: this.configService.get('AWS_PUBLIC_BUCKET_NAME'),
+        Key: pathname,
+      })
+      .promise();
   }
 }
