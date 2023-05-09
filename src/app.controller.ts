978a1191eb0fe4071033065c69e2327b9d616d9b
MuhammadAliGabol <aligabol2002@gmail.com>
2023-05-09 18:14:37 +0500

    refactor: move firebase-admin initializeApp to app controller, To avoid initializing twice when using notification service in multiple modules

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,13 @@
+import { initializeApp, cert } from 'firebase-admin/app';
 import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+    initializeApp({
+      credential: cert(JSON.parse(process.env.GOOGLE_ACCOUNT_CREDENTIALS)),
+    });
+  }
 }
