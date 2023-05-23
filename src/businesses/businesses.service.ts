0686e70264b6e24ec9cb0bcb514618fbfae596ae
MuhammadAliGabol <aligabol2002@gmail.com>
2023-05-23 18:40:29 +0500

    chore: delete thumbnail and gallery images on deleting multiple businesses

--- src/businesses/businesses.service.ts
+++ src/businesses/businesses.service.ts
@@ -9,6 +9,7 @@ import {
   UpdateReviewUserDTO,
   UpdateOwnerDTO,
 } from './business.dto';
+import { FilesService } from '../files/files.service';
 
 interface FindAllArgs {
   query: Partial<Business | { 'reviews.owner._id': string }>;
@@ -20,7 +21,8 @@ interface FindAllArgs {
 export class BusinessesService {
   constructor(
     @InjectModel(Business.name) private businessModel: Model<Business>,
-  ) {}
+    private readonly filesService: FilesService,
+  ) { }
 
   async create(
     createBusinessDTO: CreateBusinessDTO,
@@ -224,6 +226,29 @@ export class BusinessesService {
   }
 
   async removeManyByUser(userId: string): Promise<{ deletedCount?: number }> {
+
+    const businesses = await this.findAll({
+      query: { ownerId: userId.toString() },
+      projection: {},
+      options: {},
+    });
+
+    if (businesses?.length) {
+      businesses.forEach(business => {
+        if (business.thumbnail) {
+          const thumbnailURL = new URL(business.thumbnail);
+          this.filesService.deletePublicFile(thumbnailURL.pathname.replace(/^\/|\/$/g, ''));
+        }
+
+        if (business.gallery.length) {
+          business.gallery.forEach(image => {
+            const galleryImageURL = new URL(image.image);
+            this.filesService.deletePublicFile(galleryImageURL.pathname.replace(/^\/|\/$/g, ''));
+          });
+        }
+      })
+    }
+
     return this.businessModel.deleteMany({ ownerId: userId }).exec();
   }
 
