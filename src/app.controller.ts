01d8bc6b232795e4c85a908e65f13b7a8d6a6c03
Muhammad Furqan <furqan.khanzada@gmail.com>
2023-12-05 15:24:05 +0500

    Enhancement/email service (#87)
    * chore: remove unused module
    
    * chore: move emails import to its module
    
    * chore: add send forgot password mail method
    
    * chore: add send forgot password mail method
    
    * chore: configure handelbar
    
    * chore: implement forgot email

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,6 @@
 import { initializeApp, cert } from 'firebase-admin/app';
 import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
 import { PlacesModule } from './places/places.module';
 import { BusinessesModule } from './businesses/businesses.module';
 import { TagsModule } from './tags/tags.module';
 import { FilesModule } from './files/files.module';
 import { EmailModule } from './email/email.module';
-import { MailerModule } from '@nestjs-modules/mailer';
 import { NotificationsModule } from './notifications/notifications.module';
 import { NotificationUsersModule } from './notification-users/notification-users.module';
 import { DevicesModule } from './devices/devices.module';
@@ -23,21 +23,9 @@ import { InvitationModule } from './invitation/invitation.module';
       isGlobal: true,
       envFilePath: ['.env.development.local'],
     }),
-    MongooseModule.forRoot(`mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.sk8if.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`),
-    MailerModule.forRoot({
-      transport: {
-        host: process.env.SESHOST,
-        port: 465,
-        secure: true, // true for 465, false for other ports
-        auth: {
-          user: process.env.SESUSER,
-          pass: process.env.SESPASSWORD,
-        },
-      },
-      defaults: {
-        from: process.env.FROM,
-      }
-    }),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.sk8if.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`,
+    ),
     CategoriesModule,
     AuthModule,
     UsersModule,
@@ -49,7 +37,7 @@ import { InvitationModule } from './invitation/invitation.module';
     NotificationsModule,
     NotificationUsersModule,
     DevicesModule,
-    InvitationModule
+    InvitationModule,
   ],
   controllers: [AppController],
   providers: [AppService],
