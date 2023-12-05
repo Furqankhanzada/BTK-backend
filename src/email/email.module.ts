07644e7229741e16d3e6c9cf270990e871c09547
Muhammad Furqan <furqan.khanzada@gmail.com>
2023-12-05 15:16:42 +0500

    chore: move emails import to its module

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,7 +1,34 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+
 import { EmailService } from './email.service';
 
 @Module({
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.SESHOST,
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+          user: process.env.SESUSER,
+          pass: process.env.SESPASSWORD,
+        },
+      },
+      defaults: {
+        from: process.env.FROM,
+      },
+      template: {
+        dir: join(__dirname, 'templates'),
+        adapter: new HandlebarsAdapter(), // or new PugAdapter() or new EjsAdapter()
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
   providers: [EmailService],
   exports: [EmailService],
 })
